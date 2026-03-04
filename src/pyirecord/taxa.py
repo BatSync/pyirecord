9bc41b8fa6f066c554c82fbe00f6f3b0bffd1740
Jo Walsh <jowals@ceh.ac.uk>
2026-03-04 14:19:56 +0000

    add the taxon search interface (to link bat IDs and create records)

--- src/pyirecord/taxa.py
+++ src/pyirecord/taxa.py
@@ -6,14 +6,48 @@ from pyirecord.http import headers
 logging.basicConfig(level=logging.INFO)
 
 
-def taxa_search(access_token: str):
-    """Returns a raw list of surveys accessible to the current user"""
-    suffix = "/index.php/services/rest/taxa"
+def validate_params(**params):
+    """Check our generic dictionary of search parameters uses good terms."""
+    # This one is required
+    if "taxon_list_id" not in params:
+        params["taxon_list_id"] = default_taxon_list()
+
+    # TODO add any more rules (e.g. about matching terms in lists) if needed
+    # Delete any keys not appearing in the list
+    return params
+
+
+def default_taxon_list():
+    """If the query does not specify a taxon_list_id, use a default.
+    15 is the UK Species Inventory.
+    Note its encoded as string type in the JSON back and forth
+    """
+    return "15"
+
+
+def taxa_search(access_token: str, **params):
+    """Searches taxonomy to find a number ID to use creating occurrences.
+    Any named arguments are passed through to the search query, validated first.
+    See https://warehouse1.indicia.org.uk/index.php/services/rest#collapse-19
+    (We could spell them all out here, with default None values)
+
+    Without arguments other than auth token, returns 100 random records from
+    the UK Species Inventory
+    """
+
+    suffix = "/index.php/services/rest/taxa/search"
     url = f"{BASE_URL}{suffix}"
-    # TODO figure out how we supply a name as query param - see notes.md
+    params = validate_params(**params)
     try:
-        response = requests.get(url, headers=headers(access_token))
+        response = requests.get(url, headers=headers(access_token), params=params)
     except Exception as err:
         logging.error(err)
         raise
-    return response
+
+    response.raise_for_status()
+    result = response.json()
+    data = []
+    if "data" in result and len(result["data"]):
+        data = result["data"]
+
+    return data
--- test/test_taxa.py
+++ test/test_taxa.py
@@ -3,6 +3,5 @@ from pyirecord.taxa import taxa_search
 
 def test_taxa(jwt):
     s_id = taxa_search(jwt)
-    print(s_id.status_code)
-    print(s_id.json())
     assert s_id is not None
+    print(s_id)
