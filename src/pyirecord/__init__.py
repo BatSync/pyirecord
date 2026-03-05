331c62f55e99cd16e9e0b0e5abeef0ae82b073fe
Jo Walsh <jowals@ceh.ac.uk>
2026-03-05 15:49:39 +0000

    use the same base module export style as pyinat

---
+++ src/pyirecord/__init__.py
@@ -0,0 +1,8 @@
+from pyirecord.auth import jwt_token as jwt_token
+from pyirecord.surveys import survey_id as survey_id
+from pyirecord.attributes import attributes as attributes
+from pyirecord.attributes import attribute_terms as attribute_terms
+from pyirecord.media import media_queue as media_queue
+from pyirecord.observations import create_observation as create_observation
+from pyirecord.observations import get_observation as get_observation
+from pyirecord.taxa import taxa_search as taxa_search
